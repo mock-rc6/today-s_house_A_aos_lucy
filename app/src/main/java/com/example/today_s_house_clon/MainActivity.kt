980d41aa3fb9184ca61438a97abe46c79397ab53
ttoyoung <lsy9380@naver.com>
2022-06-26 01:43:32 +0900

    first commit

---
+++ app/src/main/java/com/example/today_s_house_clon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.today_s_house_clon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
