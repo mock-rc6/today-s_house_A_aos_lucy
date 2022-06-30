1847246d5dcc849d1df0e1c209e73518da5b56cb
ttoyoung <lsy9380@naver.com>
2022-07-01 00:42:59 +0900

    sign up via email Activity 완료

---
+++ app/src/main/java/com/example/today_s_house_clon/main/login/JoinViaEmailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.today_s_house_clon.main.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinViaEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_via_email)
+    }
+}
