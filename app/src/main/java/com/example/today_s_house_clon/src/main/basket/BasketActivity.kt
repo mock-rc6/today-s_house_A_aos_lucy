1f4cd3ede1cfeeb7debdcccf1bcd8a0d143822ad
ttoyoung <lsy9380@naver.com>
2022-07-08 05:27:59 +0900

    장바구니 UI완료, 상품 상세페이지 api 연동 오류

---
+++ app/src/main/java/com/example/today_s_house_clon/src/main/basket/BasketActivity.kt
@@ -0,0 +1,16 @@
+package com.example.today_s_house_clon.src.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.today_s_house_clon.R
+import com.example.today_s_house_clon.config.BaseActivity
+import com.example.today_s_house_clon.databinding.ActivityBasketBinding
+
+class BasketActivity :BaseActivity<ActivityBasketBinding>(ActivityBasketBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+}
